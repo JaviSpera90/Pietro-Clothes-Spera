8932f2e75e95a664f224afd4a977ebfa07f123ee
Javier Spera <sperajavier@hotmail.com>
2022-10-02 12:51:02 -0300

    Await

--- Pages/js/main.js
+++ Pages/js/main.js
@@ -7,12 +7,12 @@ function producto (nombre, precio) {
     }
 }
 
-const producto1 = new producto ("Jogger", 6500)
-const producto2 = new producto ("Hoodie", 7500)
-const producto3 = new producto ("Zapatillas", 15000)
-const producto4 = new producto ("Gorra", 4000)
-const producto5 = new producto ("Remera", 4500)
-const producto6 = new producto ("Campera", 20000)
+// const producto1 = new producto ("Jogger", 6500)
+// const producto2 = new producto ("Hoodie", 7500)
+// const producto3 = new producto ("Zapatillas", 15000)
+// const producto4 = new producto ("Gorra", 4000)
+// const producto5 = new producto ("Remera", 4500)
+// const producto6 = new producto ("Campera", 20000)
 
 let total = 0;
 const Carrito = []
@@ -21,18 +21,18 @@ let comprar = true;
 producto();
 
 producto= [];
-producto.push(producto1,producto2,producto3,producto4,producto5,producto6)
+// producto.push(producto1,producto2,producto3,producto4,producto5,producto6)
 
-const productos= [
-    { id: 1, nombre: "Hoodie", precio: 7500, img: "hoodie.jpg" },
-    { id: 2, nombre: "Zapatillas", precio: 15000, img: "zapatillas.jpg" },
-    { id: 3, nombre: "Remera", precio: 4500, img: "remera.jpg" },
-    { id: 4, nombre: "Campera", precio: 20000, img: "campera.jpg" },
-    { id: 5, nombre: "Gorra", precio: 4000, img: "gorra.jpg" },
-    { id: 6, nombre: "Jogger", precio: 6500, img: "jogger.jpg" },
-];
+// const productos= [
+//     { id: 1, nombre: "Hoodie", precio: 7500, img: "hoodie.jpg" },
+//     { id: 2, nombre: "Zapatillas", precio: 15000, img: "zapatillas.jpg" },
+//     { id: 3, nombre: "Remera", precio: 4500, img: "remera.jpg" },
+//     { id: 4, nombre: "Campera", precio: 20000, img: "campera.jpg" },
+//     { id: 5, nombre: "Gorra", precio: 4000, img: "gorra.jpg" },
+//     { id: 6, nombre: "Jogger", precio: 6500, img: "jogger.jpg" },
+// ];
 
-console.log(productos);
+// console.log(productos);
 document.title = "Pietro Clothes";
 
 const containerDiv = document.querySelector(".container");
@@ -40,9 +40,21 @@ const carritoDiv = document.querySelector(".carrito");
 const totalCarritoDiv = document.querySelector(".totalCarrito");
 let carrito = JSON.parse(localStorage.getItem('carrito')) || []
 
-crearCards();
-function crearCards(){
-    productos.forEach(element=>{
+const productos = [];
+const respuesta = async ()=>{
+    const response = await fetch ("/Pages/js/data.json");
+    const data = await response.json();
+    data.forEach(element=>{
+        productos.push(element);
+    })
+    crearCards(data);
+}
+respuesta();
+
+
+function crearCards(arrayConProductos){
+    // containerDiv.innerHTML = "";
+    arrayConProductos.forEach(element=>{
         let {nombre, precio, img, id} = element
         containerDiv.innerHTML += `<div style="padding: 20px; background-color:white; border: 2px solid black;">
         <h4>${nombre}</h4>
@@ -51,7 +63,7 @@ function crearCards(){
         <button class="btnCarrito" id="btn-agregar${id}">Agregar</button>
         </div>`
     })
-    agregarFuncionAlBoton();
+    agregarFuncionAlBoton(arrayConProductos);
 }
 
 function agregarFuncionAlBoton(){
